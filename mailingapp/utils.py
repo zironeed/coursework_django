0f9a32ca670070dc39b3c5db21437a9b09bd723c
Adil S <forever@nzironeed.ru>
2023-07-11 21:13:36 +0500

    add requirements.txt

--- mailingapp/utils.py
+++ mailingapp/utils.py
@@ -1,7 +1,5 @@
 from django.core.mail import send_mail
 from django.conf import settings
-import datetime
-from datetime import date
 
 
 def sendmail(to, subject, message):
@@ -10,7 +8,3 @@ def sendmail(to, subject, message):
               settings.EMAIL_HOST_USER,
               [to],
               fail_silently=False)
-
-
-time_now = datetime.datetime.now().isoformat()
-print(time_now)
