f86790ce1a24fa478b18019b4261e3d4f58f98dc
Adil S <forever@nzironeed.ru>
2023-07-11 12:33:30 +0500

    First commit

---
+++ mailingapp/urls.py
@@ -0,0 +1,11 @@
+from mailingapp.apps import MailingappConfig
+from django.urls import path
+
+#from Mailingapp.views import home
+
+app_name = MailingappConfig.name
+
+urlpatterns = [
+    #path('', home, name='home'),
+
+]
