967207fda8508018e080a39beb9838aa7d10491f
Adil S <forever@nzironeed.ru>
2023-07-11 18:43:28 +0500

    update urls.py

--- mailingapp/urls.py
+++ mailingapp/urls.py
@@ -8,17 +8,17 @@ from mailingapp.views import MainListView, ClientListView, ClientCreateView, Cli
 app_name = MailingappConfig.name
 
 urlpatterns = [
-    path('', MainListView.as_view, name='main'),
+    path('', MainListView.as_view(), name='main'),
 
-    path('clients/', ClientListView.as_view, name='clients'),
-    path('clients/create', ClientCreateView.as_view, name='client_create'),
-    path('clients/delete', ClientDeleteView.as_view, name='client_delete'),
+    path('clients/', ClientListView.as_view(), name='clients'),
+    path('clients/create', ClientCreateView.as_view(), name='client_create'),
+    path('clients/delete', ClientDeleteView.as_view(), name='client_delete'),
 
-    path('messages/', MessageListView.as_view, name='messages'),
-    path('messages/create', MessageCreateView.as_view, name='message_create'),
-    path('messages/delete', MessageDeleteView.as_view, name='message_delete'),
+    path('messages/', MessageListView.as_view(), name='messages'),
+    path('messages/create', MessageCreateView.as_view(), name='message_create'),
+    path('messages/delete', MessageDeleteView.as_view(), name='message_delete'),
 
-    path('mailings/', MailingSettingsListView.as_view, name='mailings'),
-    path('mailings/create', MailingSettingsCreateView.as_view, name='mailingsettings_create'),
-    path('mailings/delete', MailingSettingsDeleteView.as_view, name='mailingssettings_delete'),
+    path('mailings/', MailingSettingsListView.as_view(), name='mailings'),
+    path('mailings/create', MailingSettingsCreateView.as_view(), name='mailingsettings_create'),
+    path('mailings/delete', MailingSettingsDeleteView.as_view(), name='mailingssettings_delete'),
 ]
