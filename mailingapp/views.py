7ed91cbd66cf84cd5ce0f6f94d20501b33e9e325
Adil S <forever@nzironeed.ru>
2023-07-11 12:55:43 +0500

    add views

--- mailingapp/views.py
+++ mailingapp/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DeleteView
 
-# Create your views here.
+from mailingapp.models import Client, Message, MailingSettings
+
+
+class MainListView(ListView):
+    model = Message
+
+
+class ClientCreateView(CreateView):
+    model = Client
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+
+
+class MessageCreateView(CreateView):
+    model = Message
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+
+
+class MailingSettingsCreateView(CreateView):
+    model = MailingSettings
+
+
+class MailingSettingsListView(ListView):
+    model = MailingSettings
+
+
+class MailingSettingsDeleteView(DeleteView):
+    model = MailingSettings
