014317654e81ca4962204e4c15e4ef2f5ec74847
Adil S <forever@nzironeed.ru>
2023-07-13 18:42:09 +0500

    update views.py for users and mailingapp

--- mailingapp/views.py
+++ mailingapp/views.py
@@ -1,21 +1,47 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DeleteView, DetailView
-from datetime import datetime, timezone
+from django.views.generic import CreateView, ListView, DeleteView, DetailView, UpdateView
+from datetime import datetime
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
 
-import pytz
+import config.settings
 
 from mailingapp.utils import sendmail
 from mailingapp.models import Client, Message, MailingSettings, Statistic
 from mailingapp.forms import ClientCreateForm, MessageCreateForm, MailingSettingsCreateForm, StatisticForm
+from blog.models import Blog
 
 
-class MainListView(ListView):
+class MainListView(LoginRequiredMixin, ListView):
     model = Message
     template_name = "mailingapp/main.html"
+
+    def get_queryset(self):
+
+        if config.settings.CACHE_ENABLED:
+            key = 'main_blog'
+            cache_data = cache.get(key)
+
+            if cache_data is None:
+                cache_data = Blog.objects.order_by('?')[:3]
+                cache.set(key, cache_data)
+
+        else:
+            cache_data = Blog.objects.order_by('?')[:3]
+
+        return cache_data
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["Title"] = "Main"
+
+        context["Blog"] = self.get_queryset()
+
+        context["all_mailingsettings"] = len(MailingSettings.objects.all())
+        context["active_mailingsettings"] = len(MailingSettings.objects.filter(is_published=True))
+        context["all_client"] = len(Client.objects.all())
+        context["unique_client"] = len(Client.objects.all().values('email').distinct())
+
         return context
 
 
@@ -26,19 +52,70 @@ class ClientCreateView(CreateView):
     def get_context_data(self, *, object_list=None, context_object_name=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["Title"] = "Add New Client"
+
         return context
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('mailingapp:clients')
 
+    def form_valid(self, form):
+
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+
+        return super().form_valid(form)
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ["full_name", "comment", "email"]
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Title"] = "Update Client"
+
+        return context
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('mailingapp:clients')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
 
 class ClientListView(ListView):
     model = Client
 
+    def get_queryset(self):
+        queryset = super().get_queryset().all()
+
+        if not self.request.user.is_staff:
+            queryset = super().get_queryset().filter(owner=self.request.user)
+
+        return queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["Title"] = "Clients"
-        context["Clients"] = Client.objects.all()
+        context["Title"] = "Client"
+        context["Client"] = self.get_queryset()
+
+        return context
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+    def get_object(self, queryset=None):
+        one_client = super().get_object()
+
+        return one_client
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Title"] = "Client Full Information"
+        context["Client"] = self.get_object()
+
         return context
 
 
@@ -48,6 +125,7 @@ class ClientDeleteView(DeleteView):
     def get_context_data(self, *, object_list=None, context_object_name=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["Title"] = "Delete Client"
+
         return context
 
     def get_success_url(self, **kwargs):
@@ -61,20 +139,68 @@ class MessageCreateView(CreateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["Title"] = "Create Message Template"
+
         return context
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('mailingapp:messages')
 
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+
+        return super().form_valid(form)
+
 
 class MessageListView(ListView):
     model = Message
 
+    def get_queryset(self):
+        queryset = super().get_queryset().all()
+
+        if not self.request.user.is_staff:
+            queryset = super().get_queryset().filter(owner=self.request.user)
+
+        return queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["Title"] = "Messages"
-        context["Messages"] = Message.objects.all()
+        context["Title"] = "Message"
+        context["Message"] = self.get_queryset
+
         return context
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+    def get_object(self, queryset=None):
+        one_message = super().get_object()
+        return one_message
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Title"] = "Message Full Information"
+        context["Message"] = self.get_object()
+
+        return context
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ["title", "body"]
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Title"] = "Update Message"
+        return context
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('mailingapp:messages')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
 class MessageDeleteView(DeleteView):
@@ -105,33 +231,22 @@ class MailingSettingsCreateView(CreateView):
 
         # Create default data statistic
         current_mailing = self.object
-        print(current_mailing)
-        print()
         self.object = form.save()
 
+        self.object.owner = self.request.user
+        self.object.save()
+
         Statistic.objects.create(mailing_id=self.object.pk)
+
         schedule_mailing_time = self.object.time
         current_time = datetime.now().time()
-        print(schedule_mailing_time, current_time)
 
         if schedule_mailing_time <= current_time:
+
             send_message = self.object.message.get_info()
-            print("!SEND MESSAGE!")
-
-            for client in self.object.clients.all():
-
-                print(client.email)
-                print(client)
-                sendmail(client.email, send_message[0], send_message[1])
-
-            current_time = datetime.now(pytz.timezone('Europe/Moscow'))
-            print(current_time)
-
-            wu = Statistic.objects.get(mailing_id=self.object.pk)
-            wu.status = "FINISHED"
-            wu.mail_answer = "OK"
-            wu.time = datetime.now(pytz.timezone('Europe/Moscow'))
-            wu.save()
+            sendmail(self.object.pk, self.object.clients.all(), send_message[0], send_message[1])
+            self.object.status = "FINISHED"
+            self.object.save()
 
         return super().form_valid(form)
 
@@ -139,10 +254,19 @@ class MailingSettingsCreateView(CreateView):
 class MailingSettingsListView(ListView):
     model = MailingSettings
 
+    def get_queryset(self):
+        queryset = super().get_queryset().all()
+
+        if not self.request.user.is_staff:
+            queryset = super().get_queryset().filter(owner=self.request.user)
+
+        return queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["Title"] = "MailingSettings"
         context["MailingSettings"] = MailingSettings.objects.all()
+
         return context
 
 
@@ -157,10 +281,40 @@ class MailingSettingsDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["Title"] = "Mailing Full Information"
         current_object = self.get_object()
+
         context["Mailing"] = current_object
         context["Statistic"] = current_object.get_statistic[0]
-        print(current_object.get_statistic[0])
+
         return context
+
+
+class MailingSettingsUpdateView(UpdateView):
+    model = MailingSettings
+    fields = ["title", "time", "frequency", "message", "client", "is_published"]
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Title"] = "Update Mailing"
+        return context
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('mailingapp:mailings')
+
+    def form_valid(self, form):
+        schedule_mailing_time_update = form.cleaned_data["time"]
+        current_time = datetime.now().time()
+
+        self.object.status = "CREATED"
+        self.object.save()
+
+        if schedule_mailing_time_update <= current_time and self.object.is_published is True:
+            message_data = self.object.message.get_info()
+            sendmail(self.object.pk, self.object.clients.all(), message_data[0], message_data[1])
+
+            self.object.status = "FINISHED"
+            self.object.save()
+
+        return super().form_valid(form)
 
 
 class MailingSettingsDeleteView(DeleteView):
@@ -168,8 +322,9 @@ class MailingSettingsDeleteView(DeleteView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["Title"] = "Delete Transmission"
+        context["Title"] = "Delete Mailing"
+
         return context
 
     def get_success_url(self, **kwargs):
-        return reverse_lazy('mailing:transmissions')
+        return reverse_lazy('mailingapp:mailings')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,114 @@
-from django.shortcuts import render
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import LoginView as BaseLoginView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
+from django.views.generic import CreateView, UpdateView, TemplateView
+from django.urls import reverse_lazy
+import config.settings
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import Users
+from mailingapp.utils import sendmail_registration
+from django.shortcuts import redirect
+from django.contrib.auth import login
 
-# Create your views here.
+
+class TitleMixin(object):
+    """Mixin for show title on pages"""
+    title = None
+
+    def get_title(self):
+        return self.title
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Title'] = self.get_title()
+        return context
+
+
+class LoginView(TitleMixin, BaseLoginView):
+    template_name = "users/login.html"
+    title = "Login"
+
+
+class LogoutView(BaseLogoutView):
+    template_name = "users/login.html"
+
+
+class RegisterView(TitleMixin, CreateView):
+    form_class = UserRegisterForm
+    template_name = "users/registration/registration_form.html"
+    success_url = reverse_lazy('users:registration_reset')
+    title = "Register New User"
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        user.save()
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        activation_url = reverse_lazy('users:confirm_email', kwargs={'uidb64': uid, 'token': token})
+        current_site = config.settings.SITE_NAME
+        sendmail_registration(
+            user.email,
+            "Registration on Site!",
+            f"Accept your email address. Go on: http://{current_site}{activation_url}"
+        )
+        return redirect('users:email_confirmation_sent')
+
+
+class UserConfirmationSentView(PasswordResetDoneView):
+    template_name = "users/registration/registration_sent_done.html"
+
+
+class UserConfirmEmailView(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64)
+            user = Users.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, Users.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            login(request, user)
+            return redirect('users:email_confirmed')
+        else:
+            return redirect('users:email_confirmation_failed')
+
+
+class UserConfirmedView(TitleMixin, TemplateView):
+    template_name = 'users/registration/registration_confirmed.html'
+    title = "Your email is activated."
+
+
+class UserUpdateView(UpdateView):
+    model = Users
+    success_url = reverse_lazy("users:profile")
+    form_class = UserProfileForm
+    template_name = "users/profile.html"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserResetView(PasswordResetView):
+    template_name = "users/registration/password_reset_form.html"
+    email_template_name = "users/registration/password_reset_email.html"
+    success_url = reverse_lazy('users:password_reset_done')
+
+
+class UserResetDoneView(PasswordResetDoneView):
+    template_name = "users/registration/password_reset_done.html"
+
+
+class UserResetConfirmView(PasswordResetConfirmView):
+    template_name = "users/registration/password_reset_confirm.html"
+    success_url = reverse_lazy("users:password_reset_complete")
+
+
+class UserResetCompleteView(PasswordResetCompleteView):
+    template_name = "users/registration/password_reset_complete.html"
