62997fc0241019ebfdc233fa221e9a8783e49946
Adil S <forever@nzironeed.ru>
2023-07-13 18:42:34 +0500

    update utils.py for users and mailingapp

--- mailingapp/urls.py
+++ mailingapp/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 
 from mailingapp.views import MainListView, ClientListView, ClientCreateView, ClientDeleteView, \
     MessageListView, MessageCreateView, MessageDeleteView, \
-    MailingSettingsListView, MailingSettingsCreateView, MailingSettingsDeleteView
+    MailingSettingsListView, MailingSettingsCreateView, MailingSettingsDeleteView, \
+    ClientDetailView, ClientUpdateView, MessageDetailView, MessageUpdateView, MailingSettingsUpdateView, \
+    MailingSettingsDetailView
 
 app_name = MailingappConfig.name
 
@@ -12,13 +14,19 @@ urlpatterns = [
 
     path('clients/', ClientListView.as_view(), name='clients'),
     path('clients/create', ClientCreateView.as_view(), name='client_create'),
-    path('clients/delete', ClientDeleteView.as_view(), name='client_delete'),
+    path('clients/delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+    path('clients/update/<int:pk>', ClientUpdateView.as_view(), name='client_update'),
+    path('clients/card/<int:pk>', ClientDetailView.as_view(), name='clients_card'),
 
     path('messages/', MessageListView.as_view(), name='messages'),
     path('messages/create', MessageCreateView.as_view(), name='message_create'),
-    path('messages/delete', MessageDeleteView.as_view(), name='message_delete'),
+    path('messages/delete/<int:pk>', MessageDeleteView.as_view(), name='message_delete'),
+    path('messages/update/<int:pk>', MessageUpdateView.as_view(), name='message_update'),
+    path('messages/card/<int:pk>', MessageDetailView.as_view(), name='message_card'),
 
     path('mailings/', MailingSettingsListView.as_view(), name='mailings'),
     path('mailings/create', MailingSettingsCreateView.as_view(), name='mailingsettings_create'),
-    path('mailings/delete', MailingSettingsDeleteView.as_view(), name='mailingssettings_delete'),
+    path('mailings/delete/<int:pk>', MailingSettingsDeleteView.as_view(), name='mailingssettings_delete'),
+    path('mailings/update/<int:pk>', MailingSettingsUpdateView.as_view(), name='mailingssettings_update'),
+    path('mailings/card/<int:pk>', MailingSettingsDetailView.as_view(), name='mailingssettings_card'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
+from users.views import LoginView, LogoutView, RegisterView, UserUpdateView, UserResetView, \
+    UserResetCompleteView, UserResetConfirmView, UserResetDoneView, UserConfirmationSentView, UserConfirmEmailView, \
+    UserConfirmedView
+
 
 app_name = "users"
 
 urlpatterns = [
-    #path("", LoginView.as_view(), name="login"),
+    path("", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
+    path("registration/", RegisterView.as_view(), name="registration"),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
+
+    path("profile/", UserUpdateView.as_view(), name="profile"),
+
+    path('password_reset/', UserResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', UserResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', UserResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/complete/', UserResetCompleteView.as_view(), name='password_reset_complete')
 ]
